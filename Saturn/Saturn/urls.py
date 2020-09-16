5c01105a7fa36cc3b49f6863181752d8e153f927
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2020-09-16 13:28:18 -0300

    Esquelo del registro completo

--- Saturn/Saturn/urls.py
+++ Saturn/Saturn/urls.py
@@ -19,9 +19,9 @@ from users import views
 
 urlpatterns = [
     path('', views.welcome),
-    path('register', views.register),
-    path('login', views.login),
-    path('logout', views.logout),
+    path('register/', views.register),
+    path('login/', views.login),
+    path('logout/', views.logout),
 
     path('admin/', admin.site.urls),
 ]
