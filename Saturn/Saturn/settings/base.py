dfcb942764cc3ae1b880f67b63ffc8edecce3d49
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2020-09-16 12:44:07 -0300

    Cambios minimos

--- Saturn/Saturn/settings/base.py
+++ Saturn/Saturn/settings/base.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'Saturn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
