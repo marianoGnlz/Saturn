6ac6bc3df888c6269f7d1aee6467676a7a5ff308
Cesar Herrera <lakatos3697@gmail.com>
2020-09-18 01:48:14 -0300

    weitas

--- Saturn/Saturn/urls.py
+++ Saturn/Saturn/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('turnos/', views.turnos, name='turnos'),
     path('admin/', admin.site.urls),
 ]
