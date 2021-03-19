f9d6fb8782539a954e6eab6c463ce8b76da2c425
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-03-19 19:55:20 -0300

    Update base.py

--- Saturn/Saturn/settings/base.py
+++ Saturn/Saturn/settings/base.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Saturn.wsgi.application'
 
 
 # Database
-https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
